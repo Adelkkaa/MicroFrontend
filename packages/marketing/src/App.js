bcc24ed2b9edda87208d45e91b1a0b0562237850
Adel <snakeadel00@gmail.com>
2024-04-11 10:40:30 +0300

    initial commit

---
+++ packages/marketing/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StylesProvider } from "@material-ui/core";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+export const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+    {
+      path: "/pricing",
+      element: <Pricing />,
+    },
+    {
+      path: "*",
+      element: <>Хуй</>,
+    },
+  ]);
+  return (
+    <StylesProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </StylesProvider>
+  );
+};
